e907aa4a0e2724181a058fe272dcb11fef5b3789
Sebastian Gorr <sebastian.gorr@promantis.de>
2026-03-01 11:17:47 +0100

    fix: update admin user language from English to German in seed script

--- apps/api/src/prisma/seed.js
+++ apps/api/src/prisma/seed.js
@@ -17,7 +17,7 @@ async function main() {
         isAdmin: true,
         password:
           "$2b$10$BFmibvOW7FtY0soAAwujoO9y2tIyB7WEJ2HNq9O7zh9aeejMvRsKu",
-        language: "en",
+        language: "de",
       },
     });
 
